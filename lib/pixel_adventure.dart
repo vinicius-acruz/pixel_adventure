e8585e96a75e0b432a97bb670aa4fb09354e0275
vinicius-acruz <127705058+vinicius-acruz@users.noreply.github.com>
2023-09-11 12:09:42 -0300

    Second commit - project setup

---
+++ lib/pixel_adventure.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'package:flame/camera.dart';
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:pixel_adventure/levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+  late final CameraComponent cam;
+  final world = Level();
+
+  @override
+  FutureOr<void> onLoad() {
+    cam = CameraComponent.withFixedResolution(
+        world: world, width: 640, height: 360);
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
+    return super.onLoad();
+  }
+}
